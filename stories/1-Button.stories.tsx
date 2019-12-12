c135f8805a57e3f884f7d3acf5e3541084e8d7d3
nukosuke <nukosuke@users.noreply.github.com>
2019-12-12 20:23:07 +0900

    Setup storybook (#3)
    * Fix package.json
    
    * Exec: sb init --type react
    
    * Setup TypeScript preset
    
    * Add devDependencies
    
      - ts-loader
      - @storybook/preset-typescript
      - react-docgen-typescript-loader
    
    * Add .storybook/presets.js
    
    * TSfy stories

---
+++ stories/1-Button.stories.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'Button',
+};
+
+export const text = () => <button onClick={action('clicked')}>Hello Button</button>;
+
+export const emoji = () => (
+  <button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool"></span>
+  </button>
+);
